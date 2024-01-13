6b7cff3031b22571c4f4618828ba95b55e775055
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-13 18:12:54 +0530

    add some changes

--- api/account.go
+++ api/account.go
@@ -17,8 +17,8 @@ type GetAccountRequest struct {
 	ID int64 `uri:"id" binding:"required, min=1"`
 }
 type ListAccountRequest struct {
-	PageID   int32 `form:"page_id" binding:"required, min=1"`
-	PageSize int32 `form:"page_size" binding:"required, min=5, max=10"`
+	PageID   int32 `form:"page_id" binding:"required,gte=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
