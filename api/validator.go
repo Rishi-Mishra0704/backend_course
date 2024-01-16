cb46c01c8f663bb8aca070dfd6a99367f62f1aa1
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-16 15:34:28 +0530

    add custom currency validator

---
+++ api/validator.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"github.com/Rishi-Mishra0704/backend_course/util"
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return util.IsSupportedCurrency(currency)
+	}
+	return false
+}
--- util/random.go
+++ util/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"USD", "EUR", "CAD", "INR"}
+	currencies := []string{USD, EUR, CAD, INR}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
