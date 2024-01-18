5f5f3fa954fa5027e59a2224d1f87b77ce69a179
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-18 12:22:00 +0530

    add create user controller

---
+++ api/user.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/Rishi-Mishra0704/backend_course/db/sqlc"
+	"github.com/Rishi-Mishra0704/backend_course/util"
+	"github.com/gin-gonic/gin"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var request CreateUserRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	hashedPassword, err := util.HashPassword(request.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       request.Username,
+		HashedPassword: hashedPassword,
+		FullName:       request.FullName,
+		Email:          request.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		errCode := db.ErrorCode(err)
+		if errCode == db.UniqueViolation {
+			ctx.JSON(http.StatusForbidden, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
