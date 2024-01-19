0a412bd5ca500eb6f1ffc959932f89d29733ec54
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-19 19:39:45 +0530

    add new config

--- api/server.go
+++ api/server.go
@@ -1,19 +1,30 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/Rishi-Mishra0704/backend_course/db/sqlc"
+	"github.com/Rishi-Mishra0704/backend_course/token"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker("")
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker %w", err)
+	}
+
+	server := &Server{
+		store:      store,
+		tokenMaker: tokenMaker}
 	router := gin.Default()
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
@@ -26,7 +37,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/users", server.createUser)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
