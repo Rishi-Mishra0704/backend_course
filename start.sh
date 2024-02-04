d8c448657154acc66a4f762d04f19a7189cd6adb
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-04 19:50:07 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
 set -e 
+
+# Load environment variables from app.env
+if [ -f app.env ]; then
+    echo "Loading environment variables from app.env"
+    set -o allexport
+    source app.env
+    set +o allexport
+else
+    echo "app.env file not found. Please make sure it exists in the current directory."
+    exit 1
+fi
+
+echo "DB_SOURCE: $DB_SOURCE"
+
 echo "run db migrations"
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
