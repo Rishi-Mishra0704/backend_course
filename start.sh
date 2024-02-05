89d678c84c7b7db0413a406fe67e085f0122a528
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-05 15:03:56 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -2,21 +2,8 @@
 
 set -e
 
-# Directly set DB_SOURCE (for testing purposes only)
-DB_SOURCE="postgresql://root:mCHF7frAgCyhgLkVFJe0@simple-bank.clsi8qk24waz.ap-south-1.rds.amazonaws.com:5432/simple_bank"
+source app/app.env
 
-# Debug print
-echo "DB_SOURCE before sourcing: $DB_SOURCE"
-
-# No need to source app.env in this case
-
-# Debug print
-echo "DB_SOURCE after sourcing: $DB_SOURCE"
-
-if [ -z "$DB_SOURCE" ]; then
-  echo "Error: DB_SOURCE is empty"
-  exit 1
-fi
 
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
