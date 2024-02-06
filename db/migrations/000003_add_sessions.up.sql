cac9c3a5eade4674d3c03feac41c33039a000e80
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-07 00:49:43 +0530

    add new migrations

--- db/migrations/000003_add_sessions.up.sql
+++ db/migrations/000003_add_sessions.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE "sessions" (
   "user_agent" varchar NOT NULL,
   "client_ip" varchar NOT NULL,
   "is_blocked" boolean NOT NULL DEFAULT false,
-  "expires_at" TIMESTAMP NOT NULL,
-  "created_at" TIMESTAMP NOT NULL DEFAULT (now())
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
